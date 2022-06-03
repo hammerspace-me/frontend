c04e9b5e6bbcd35312bdacae4b5e56e690be707c
Tobias Petrasch <tobiaspetrasch@googlemail.com>
2022-06-03 14:02:15 +0200

    remove strict mode from react due to multiple render issues

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  root.render(<App />);
 }
 
 // If you want to start measuring performance in your app, pass a function
